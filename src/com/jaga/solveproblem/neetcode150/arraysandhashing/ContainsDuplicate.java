f6a82305321fb8c2d501ae338f91b5a4f7335c5d
Jagadeesh Thangavel <jagadeeshthangavel@mac-1.home>
2026-01-17 12:51:12 +0100

    Test

--- src/com/jaga/solveproblem/neetcode150/arraysandhashing/ContainsDuplicate.java
+++ src/com/jaga/solveproblem/neetcode150/arraysandhashing/ContainsDuplicate.java
@@ -6,10 +6,13 @@ import java.util.Set;
 public class ContainsDuplicate {
 
     public static void main(String[] args) {
-        boolean containsDuplicate = containsDuplicate(new int[]{1, 2, 3, 1});
-        System.out.println(containsDuplicate);
-        boolean containsDuplicate1 = containsDuplicate(new int[]{1, 2, 3, 4});
-        System.out.println(containsDuplicate1);
+//        boolean containsDuplicate = containsDuplicate(new int[]{1, 2, 3, 1});
+//        System.out.println(containsDuplicate);
+//        boolean containsDuplicate1 = containsDuplicate(new int[]{1, 2, 3, 4});
+//        System.out.println(containsDuplicate1);
+
+        boolean containsDuplicate2 = containsDuplicateTest(new int[]{1, 2, 3, 3});
+        System.out.println(containsDuplicate2);
     }
     public static boolean containsDuplicate(int[] nums) {
         Set<Integer> store = new HashSet<>();
@@ -20,4 +23,17 @@ public class ContainsDuplicate {
         }
         return false;
     }
+
+    public static boolean containsDuplicateTest(int[] nums) {
+
+        Set<Integer> store = new HashSet<>();
+        for(int i=0;i<nums.length;i++) {
+            if(!store.add(nums[i])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
--- src/com/jaga/solveproblem/neetcode150/arraysandhashing/ValidAnagram.java
+++ src/com/jaga/solveproblem/neetcode150/arraysandhashing/ValidAnagram.java
@@ -7,12 +7,15 @@ public class ValidAnagram {
 
     public static void main(String[] args) {
 
-        boolean isAnagram = isAnagram("anagram", "nagaram");
-        System.out.println(isAnagram);
-        boolean isAnagram1 = isAnagram("cat", "rat");
-        System.out.println(isAnagram1);
-        boolean isAnagram2 = isAnagram("aacc", "ccac");
-        System.out.println(isAnagram2);
+//        boolean isAnagram = isAnagram("anagram", "nagaram");
+//        System.out.println(isAnagram);
+//        boolean isAnagram1 = isAnagram("cat", "rat");
+//        System.out.println(isAnagram1);
+//        boolean isAnagram2 = isAnagram("aacc", "ccac");
+//        System.out.println(isAnagram2);
+
+        boolean isAnagram3 = isAnagramTest("carrace", "racecar");
+        System.out.println(isAnagram3);
 
 
     }
@@ -55,6 +58,30 @@ public class ValidAnagram {
             }
             store.put(c,store.get(c)-1);
             if(store.get(c)<0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public static boolean isAnagramTest(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+
+        Map<Character, Integer> store  = new HashMap<>();
+
+        for(int i=0;i<s.length();i++) {
+            store.put(s.charAt(i), store.getOrDefault(s.charAt(i),0)+1);
+        }
+
+        for(int j=0;j<t.length();j++) {
+            if(!store.containsKey(t.charAt(j))) {
+                return false;
+            }
+
+            store.put(t.charAt(j), store.get(t.charAt(j))-1);
+
+            if(store.get(t.charAt(j))<0) {
                 return false;
             }
         }
