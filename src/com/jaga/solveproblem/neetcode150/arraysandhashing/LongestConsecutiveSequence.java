bdbf07ac80c3a0789011ee9be660ddca704f60e9
Jagadeesh Thangavel <jagadeeshthangavel@mac-1.home>
2026-01-19 12:50:01 +0100

    Test

--- src/com/jaga/solveproblem/neetcode150/arraysandhashing/LongestConsecutiveSequence.java
+++ src/com/jaga/solveproblem/neetcode150/arraysandhashing/LongestConsecutiveSequence.java
@@ -6,10 +6,13 @@ import java.util.Set;
 public class LongestConsecutiveSequence {
 
     public static void main(String[] args) {
-        int longerStreak = longestConsecutive(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1});
-        System.out.println(longerStreak);
+//        int longerStreak = longestConsecutive(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1});
+//        System.out.println(longerStreak);
         int longerStreak1 = longestConsecutive(new int[]{100,4,200,1,3,2});
         System.out.println(longerStreak1);
+
+        int longerStreak2 = longestConsecutiveTest(new int[]{100,4,200,1,3,2});
+        System.out.println(longerStreak2);
     }
 
     public static int longestConsecutive(int[] nums) {
@@ -37,5 +40,32 @@ public class LongestConsecutiveSequence {
         }
 
         return longestStreak;
+    }
+
+    public static int longestConsecutiveTest(int[] nums) {
+        Integer result=0;
+        Set<Integer> store = new HashSet<>();
+        for(Integer num : nums) {
+            store.add(num);
+        }
+
+        for(Integer num : nums) {
+            Integer currentNumber = num;
+            Integer streak=1;
+            if(!store.contains(num-1)) {
+
+                while(store.contains(currentNumber+1)) {
+                    streak++;
+                    currentNumber++;
+                }
+
+                result = Math.max(streak,result);
+
+            }
+
+        }
+
+        return result;
+
     }
 }
--- src/com/jaga/solveproblem/neetcode150/arraysandhashing/TopKFrequentElements.java
+++ src/com/jaga/solveproblem/neetcode150/arraysandhashing/TopKFrequentElements.java
@@ -1,13 +1,16 @@
 package com.jaga.solveproblem.neetcode150.arraysandhashing;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import com.jaga.solveproblem.common.MyUtil;
+
+import java.awt.event.WindowStateListener;
+import java.util.*;
 
 public class TopKFrequentElements {
 
     public static void main(String[] args) {
 
+        int[] result = topKFrequentTest(new int[]{1, 1, 1, 2, 2, 3, 4, 4, 4, 4}, 2);
+        MyUtil.print(result);
 
     }
 
@@ -34,5 +37,31 @@ public class TopKFrequentElements {
 
         return result;
 
+    }
+
+    public static int[] topKFrequentTest(int[] nums, int k) {
+
+        Map<Integer, Integer> store =  new HashMap<>();
+        for (int num : nums) {
+            store.put(num, store.getOrDefault(num, 0) + 1);
+        }
+
+       PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((a,b)->a.getValue()-b.getValue());
+       store.entrySet().forEach(entrySet -> {
+           minHeap.offer(entrySet);
+           if(minHeap.size()>k) {
+               minHeap.poll();
+           }
+       });
+
+
+        int[] result = new int[k];
+        // to be added
+
+        for(int i=0;i<k;i++) {
+            result[i] = minHeap.poll().getKey();
+        }
+
+        return result;
     }
 }
