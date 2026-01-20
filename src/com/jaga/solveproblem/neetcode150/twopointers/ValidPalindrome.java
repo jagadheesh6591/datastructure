8bb8ffc73f0342bd74ff16d84a8ace88cec37083
Jagadeesh Thangavel <jagadeeshthangavel@mac-1.home>
2026-01-20 22:15:44 +0100

    two pointer easy - validpalindrom

--- src/com/jaga/solveproblem/neetcode150/twopointers/ValidPalindrome.java
+++ src/com/jaga/solveproblem/neetcode150/twopointers/ValidPalindrome.java
@@ -1,8 +1,46 @@
 package com.jaga.solveproblem.neetcode150.twopointers;
 
+import static com.jaga.solveproblem.string.ValidPalindrom.isValidPalindrome;
+
 public class ValidPalindrome {
 
     public static void main(String[] args) {
 
+        String input = "A man, a plan, a canal: Panama";
+
+        boolean result = isValidPalindromeCheck(input);
+
+        // Test Cases
+        System.out.println(isValidPalindromeCheck("A man, a plan, a canal: Panama")); // true
+        System.out.println(isValidPalindromeCheck("race a car")); // false
+        System.out.println(isValidPalindromeCheck(" ")); // true
+        System.out.println(isValidPalindromeCheck("ab_a")); // true
+        System.out.println(isValidPalindromeCheck("0P")); // false
+
+    }
+
+    private static boolean isValidPalindromeCheck(String input) {
+
+        String lowCase = input.toLowerCase();
+        int i=0,j=lowCase.length()-1;
+
+        while(i<j) {
+
+            while(i<j&&!Character.isLetterOrDigit(lowCase.charAt(i))){
+                i++;
+            }
+            while(i<j&&!Character.isLetterOrDigit(lowCase.charAt(j))){
+                j--;
+            }
+
+            if(lowCase.charAt(i)!=lowCase.charAt(j)) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
     }
 }
